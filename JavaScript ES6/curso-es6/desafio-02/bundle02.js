f8875653b0307e14b43a5ba4e099b0e2d7280050
Thelsandro <thelsandro.antunes@gmail.com>
2019-08-18 18:36:55 -0400

    desafio02

---
+++ JavaScript ES6/curso-es6/desafio-02/bundle02.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _functions = _interopRequireDefault(require("./functions.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// 1.1
+_functions["default"].info(); // 1.2
+
+/*
+import { idade } from "./functions";
+
+console.log(idade);
+
+// 1.3
+
+import Usuario, { idade as IdadeUsuario } from "./functions";*/
---
+++ JavaScript ES6/curso-es6/desafio-02/desafio02.js
@@ -0,0 +1,17 @@
+// 1.1
+
+
+
+import ClasseUsuario from './functions.js';
+
+ClasseUsuario.info();
+
+// 1.2
+/*
+import { idade } from "./functions";
+
+console.log(idade);
+
+// 1.3
+
+import Usuario, { idade as IdadeUsuario } from "./functions";*/
