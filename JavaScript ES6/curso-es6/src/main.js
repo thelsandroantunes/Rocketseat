93428ef2d1997dde9d46414a0041e3e730651ffc
Thelsandro <39775561+thelsandroantunes@users.noreply.github.com>
2019-09-01 13:08:21 -0300

    finalizado

--- JavaScript ES6/curso-es6/src/main.js
+++ JavaScript ES6/curso-es6/src/main.js
@@ -1,8 +1,8 @@
-import  api  from "./api";
+import api from "./api";
 
 class App {
-    constructor(){
-        this.repositories =[];
+    constructor() {
+        this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
         this.inputEl = document.querySelector('input[name=repository]')
@@ -11,11 +11,23 @@ class App {
         this.registerHandlers();
     }
 
-    registerHandlers(){
-        this.formEl.onsubmit = event =>this.addRepository(event);
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    async addRepository(event){
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
         event.preventDefault();
 
         const repoInput = this.inputEl.value;
@@ -23,26 +35,31 @@ class App {
         if (repoInput.length === 0) {
             return;
         }
+        this.setLoading();
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
 
-        const response = await api.get(`/repos/${repoInput}`);
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
 
-        const {name, description, html_url, owner: {avatar_url}} = response.data;
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url,
+            });
 
-        });
+            this.inputEl.value = '';
+            console.log(this.repositories);
 
-        this.inputEl.value = '';
-        console.log(this.repositories);
-
-        this.render();
+            this.render();
+        } catch{
+            alert('O repositorio não existe!');
+        }
+        this.setLoading(false);
     }
 
-    render(){
+    render() {
         this.listEl.innerHTML = '';
 
         this.repositories.forEach(repo => {
